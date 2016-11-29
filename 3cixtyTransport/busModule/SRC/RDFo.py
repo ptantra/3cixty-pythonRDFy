8754a2477e957762ec08a178818eb470d4aa3556
cityremade <agatbr@gmail.com>
2016-11-29 17:21:17 +0000

    Added a few methods to the Bus class

--- 3cixtyTransport/busModule/SRC/RDFo.py
+++ 3cixtyTransport/busModule/SRC/RDFo.py
@@ -1,34 +1,30 @@
 #libraries
 import rdflib
-from rdflib import URIRef, Literal, Namespace, plugin, Graph
+from rdflib import URIRef, Literal, Namespace, Graph
 import csv
 
 # This is an object that can bind its prefixes
 class Tree:
     def __init__(self, g):
         self.g = g
-        self.prefixes = {'geo': 'http://www.w3.org/2003/01/geo/wgs84_pos#',
-            'foaf': 'http://xmlns.com/foaf/0.1/',
-            'geom': 'http://geovocab.org/geometry#',
-            'transit': 'http://vocab.org/transit/terms/',
-            'locn': 'http://www.w3.org/ns/locn#',
-            'vcard': 'http://www.w3.org/2006/vcard/ns#',
-            'dcterms': 'http://purl.org/dc/terms/',
-            'schema': 'http://schema.org/',
-            'geosparql': 'http://www.opengis.net/ont/geosparql#',
-            'unknown': 'http://data.linkedevents.org/def/unknown#',
-            'rdfs': 'http://www.w3.org/2000/01/rdf-schema#',
-            'dul': 'http://ontologydesignpatterns.org/ont/dul/DUL.owl#',
-            'naptan': 'http://transport.data.gov.uk/def/naptan/',
-            'xsd': 'http://www.w3.org/2001/XMLSchema#',
-            'owl': 'http://www.w3.org/2002/07/owl#',
-            'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-            'locationOnt': 'http://data.linkedevents.org/def/location#',
+        self.prefixes = {
             'dc': 'http://purl.org/dc/elements/1.1/',
-            'travel': 'http://3cixty.com/ontology#',
-            'qb': 'http://purl.org/linked-data/cube#',
             'dct': 'http://purl.org/dc/terms/',
-            'sf': 'http://www.opengis.net/ont/sf#'}
+            'dul': 'http://ontologydesignpatterns.org/ont/dul/DUL.owl#',
+            'geo': 'http://www.w3.org/2003/01/geo/wgs84_pos#',
+            'geom': 'http://geovocab.org/geometry#',
+            'geosparql': 'http://www.opengis.net/ont/geosparql#',
+            'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+            'rdfs': 'http://www.w3.org/2000/01/rdf-schema#',
+            'locationOnt': 'http://data.linkedevents.org/def/location#',
+            'locn': 'http://www.w3.org/ns/locn#',
+            'naptan': 'http://transport.data.gov.uk/def/naptan/',
+            'owl':'http://www.w3.org/2002/07/owl#',
+            'schema':'http://schema.org/',
+            'transit': 'http://vocab.org/transit/terms/',
+            'unknown': 'http://data.linkedevents.org/def/unknown#',
+            'xsd': 'http://www.w3.org/2001/XMLSchema#'
+        }
 
     def bindingPrefixes(self, g):
         self.g = g
@@ -39,43 +35,61 @@ class Tree:
 # This is an RDF superclass that knows all the namespaces
 class RDF:
     def __init__(self):
-        self.geo = Namespace("http://www.w3.org/2003/01/geo/wgs84_pos#")
-        self.foaf = Namespace("http://xmlns.com/foaf/0.1/")
-        self.geom = Namespace("http://geovocab.org/geometry#")
-        self.unknown = Namespace("http://data.linkedevents.org/def/unknown#")
-        self.transit = Namespace("http://vocab.org/transit/terms/")
-        self.locn = Namespace("http://www.w3.org/ns/locn#")
-        self.vcard = Namespace('http://www.w3.org/2006/vcard/ns#')
-        self.dcterms = Namespace("http://purl.org/dc/terms/")
-        self.schema = Namespace('http://schema.org/')
-        self.geosparql = Namespace("http://www.opengis.net/ont/geosparql#")
-        self.rdfs = Namespace('http://www.w3.org/2000/01/rdf-schema#')
-        self.naptan = Namespace('http://transport.data.gov.uk/def/naptan/')
-        self.xsd = Namespace('http://www.w3.org/2001/XMLSchema#')
-        self.owl = Namespace('http://www.w3.org/2002/07/owl#')
-        self.rdf = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
-        self.locationOnt = Namespace("http://data.linkedevents.org/def/location#")
-        self.dul = Namespace('http://ontologydesignpatterns.org/ont/dul/DUL.owl#')
         self.dc = Namespace('http://purl.org/dc/elements/1.1/')
-        self.travel = Namespace('http://3cixty.com/ontology#')
-        self.qb = Namespace('http://purl.org/linked-data/cube#')
         self.dct = Namespace('http://purl.org/dc/terms/')
-        self.sf = Namespace("http://www.opengis.net/ont/sf#")
+        self.dul = Namespace('http://ontologydesignpatterns.org/ont/dul/DUL.owl#')
+        self.geo = Namespace('http://www.w3.org/2003/01/geo/wgs84_pos#')
+        self.geom = Namespace('http://geovocab.org/geometry#')
+        self.geosparql = Namespace('http://www.opengis.net/ont/geosparql#')
+        self.rdf = Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
+        self.rdfs = Namespace('http://www.w3.org/2000/01/rdf-schema#')
+        self.locationOnt = Namespace('http://data.linkedevents.org/def/location#')
+        self.locn = Namespace('http://www.w3.org/ns/locn#')
+        self.naptan = Namespace('http://transport.data.gov.uk/def/naptan/')
+        self.owl = Namespace('http://www.w3.org/2002/07/owl#')
+        self.schema = Namespace('http://schema.org/')
+        self.transit = Namespace('http://vocab.org/transit/terms/')
+        self.unknown = Namespace('http://data.linkedevents.org/def/unknown#')
+        self.xsd = Namespace('http://www.w3.org/2001/XMLSchema#')
 
 # ------ This is the Bus class
 
 class Bus(RDF):
     def __init__(self, stopId):
         RDF.__init__(self)
-        self.stopId = stopId
+        self.stopId = stopId.replace(" ", "")
 
     def createBusStop(self):
-        busStop = URIRef("http://data.linkedevents.org/transit/London/stop/" + Literal(self.stopId).replace(" ", ""))
+        busStop = URIRef("http://data.linkedevents.org/transit/stop/" + Literal(self.stopId))
         return busStop
+
+    def createGeometry(self):
+        busStopGeom = URIRef('http://data.linkedevents.org/London/location/' + Literal(self.stopId) + '/geometry')
+        return busStopGeom
+
+    def createAddress(self):
+        stopAddress = URIRef('http://data.linkedevents.org/location/' + Literal(self.stopId) + '/address')
+        return stopAddress
+
+    def createLabel(self):
+        title = Literal(str(self.label).title())
+        return title
 
     def createBusGraph(self, g):
         busStop = self.createBusStop()
+        address = self.createAddress()
+        geom = self.createGeometry()
+        #label = Literal(self.createLabel())
+
         g.add((busStop, self.rdf.type, self.naptan.BusStop))
+        g.add((busStop, self.rdf.type, self.dul.Place))
+        g.add((busStop, self.rdf.type, self.transit.Stop))
+        g.add((busStop, self.dc.identifier, Literal(self.stopId)))
+        g.add((busStop, self.geom.geometry, geom))
+
+        g.add((busStop, self.geo.location, geom))
+        g.add((busStop, self.schema.location, address))
+
         return g
 
 # ----- This is an RDF creator
@@ -99,6 +113,7 @@ class RDFCreator:
 
 # -------- Main
 def main(content, path):
+    print "Generating file... Wait."
     tree = Tree(content)
     tree.bindingPrefixes(content)
     content.serialize(destination=path, format='turtle')
